f633f1e74995905a820c7bafd23e4bc569f0015e
Juan Diego <perezlopezjuandiego59@gmail.com>
2026-03-01 13:09:38 -0500

    Agregada ruta visual /posts

--- app/routes/views.py
+++ app/routes/views.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template
 
 views_bp = Blueprint("views", __name__)
 
-@views_bp.route("/")
-def home():
-    return render_template("index.html")
+@views_bp.route("/posts")
+def posts_page():
+    return render_template("posts.html")
+
 
 @views_bp.route("/login")
 def login_page():
